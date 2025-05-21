32bfec39e45ebb4d36e21cd4e7bc445ca4bcd73a
SamirShef <ssserin10@gmail.com>
2025-05-21 20:27:00 +0300

    Add via upload

---
+++ Core/Lexer/TokenType.cs
@@ -0,0 +1,24 @@
+﻿namespace Core.Lexer;
+
+public enum TokenType
+{
+    Identifier,
+
+    Class, Let, Func, Return,
+
+    Int, Float, Bool, String,
+    NumberLiteral, StringLiteral, BooleanLiteral,
+
+    Plus, Minus, Multiply, Divide, Modulo, Assign,
+    PlusAssign, MinusAssign, MultiplyAssign, DivideAssign, ModuloAssign,
+    Increment, Decrement, Greater, Less, GreaterEqual, LessEqual,
+    Equals, Not, NotEquals,
+
+    And, Or,
+
+    LBrace, RBrace, LParen, RParen, LBracket, RBracket,
+    
+    Semicolon, Comma, Dot, Colon,
+    
+    EOF
+}
